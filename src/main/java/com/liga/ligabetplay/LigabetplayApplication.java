14801fb8b502267d6a8fbba48277d004b576b089
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2024-11-17 01:56:00 -0500

    Initial commit

---
+++ src/main/java/com/liga/ligabetplay/LigabetplayApplication.java
@@ -0,0 +1,14 @@
+package com.liga.ligabetplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigabetplayApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LigabetplayApplication.class, args);
+	}
+
+}
