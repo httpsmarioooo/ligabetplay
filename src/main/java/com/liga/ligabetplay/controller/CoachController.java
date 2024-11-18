cc4f39ecb5ef86d6602c416cd74aaef6d6519654
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2024-11-18 00:58:24 -0500

    Coach Controller

---
+++ src/main/java/com/liga/ligabetplay/controller/CoachController.java
@@ -0,0 +1,46 @@
+package com.liga.ligabetplay.controller;
+
+import com.liga.ligabetplay.dto.CoachDTO;
+import com.liga.ligabetplay.repository.CoachRepository;
+import com.liga.ligabetplay.service.CoachService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/coach")
+public class CoachController {
+
+    private CoachRepository coachRepository;
+    private CoachService coachService;
+
+    public CoachController(CoachRepository coachRepository, CoachService coachService) {
+        this.coachRepository = coachRepository;
+        this.coachService = coachService;
+    }
+
+    @PostMapping(value = "/guardarNuevoCoach")
+    public ResponseEntity<CoachDTO> guardarNuevoCoach(@RequestBody CoachDTO coachDTO) throws Exception {
+        CoachDTO coachResponse = coachService.guardarNuevoCoach(coachDTO);
+        return new ResponseEntity<>(coachResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/obtenerCoaches")
+    public List<CoachDTO> obtenerCoaches(){
+        return coachService.obtenerCoaches();
+    }
+
+    @GetMapping("/buscarCoachPorId/{id}")
+    public ResponseEntity<CoachDTO> buscarCoachPorId (Integer id)throws Exception {
+        CoachDTO coachResponse = coachService.buscarCoachPorId(id);
+        return new ResponseEntity<>(coachResponse, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/modificarCoach")
+    public ResponseEntity<CoachDTO> modificarCoach(@RequestBody CoachDTO coachDTO) throws Exception {
+        CoachDTO coachResponse = coachService.modificarCoach(coachDTO);
+        return new ResponseEntity<>(coachResponse, HttpStatus.OK);
+    }
+}
