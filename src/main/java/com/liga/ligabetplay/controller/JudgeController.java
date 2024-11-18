895d02fdf1186d547112b4a02859950b79c885c5
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2024-11-18 01:58:04 -0500

    JudgeRole - Terminado

---
+++ src/main/java/com/liga/ligabetplay/controller/JudgeController.java
@@ -0,0 +1,42 @@
+package com.liga.ligabetplay.controller;
+
+import com.liga.ligabetplay.dto.JudgeDTO;
+import com.liga.ligabetplay.service.JudgeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/judge")
+public class JudgeController {
+    private JudgeService judgeService;
+
+    public JudgeController(JudgeService judgeService) {
+        this.judgeService = judgeService;
+    }
+
+    @PostMapping(value = "/guardarNuevoJudge")
+    public ResponseEntity<JudgeDTO> guardarNuevoJudge(@RequestBody JudgeDTO judgeDTO) throws Exception {
+        JudgeDTO judgeResponse = judgeService.guardarNuevoJudge(judgeDTO);
+        return new ResponseEntity<>(judgeResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/obtenerJudges")
+    public List<JudgeDTO> obtenerJudges(){
+        return judgeService.obtenerJudges();
+    }
+
+    @GetMapping("/buscarJudgePorId/{id}")
+    public ResponseEntity<JudgeDTO> buscarJudgePorId (Integer id)throws Exception {
+        JudgeDTO judgeResponse = judgeService.buscarJudgePorId(id);
+        return new ResponseEntity<>(judgeResponse, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/modificarJudge")
+    public ResponseEntity<JudgeDTO> modificarJudge(@RequestBody JudgeDTO judgeDTO) throws Exception {
+        JudgeDTO judgeResponse = judgeService.modificarJudge(judgeDTO);
+        return new ResponseEntity<>(judgeResponse, HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/liga/ligabetplay/service/implementation/JudgeServiceImpl.java
@@ -0,0 +1,101 @@
+package com.liga.ligabetplay.service.implementation;
+
+import com.liga.ligabetplay.domain.Judge;
+import com.liga.ligabetplay.domain.JudgeRole;
+import com.liga.ligabetplay.dto.JudgeDTO;
+import com.liga.ligabetplay.mapper.JudgeMapper;
+import com.liga.ligabetplay.repository.JudgeRepository;
+import com.liga.ligabetplay.repository.JudgeRoleRepository;
+import com.liga.ligabetplay.service.JudgeService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class JudgeServiceImpl implements JudgeService {
+
+    private final JudgeRepository judgeRepository;
+    private final JudgeRoleRepository judgeRoleRepository;
+
+    public JudgeServiceImpl(JudgeRepository judgeRepository, JudgeRoleRepository judgeRoleRepository) {
+        this.judgeRepository = judgeRepository;
+        this.judgeRoleRepository = judgeRoleRepository;
+    }
+
+
+    @Override
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+    public JudgeDTO guardarNuevoJudge(JudgeDTO judgeDTO) throws Exception {
+        if(judgeDTO.getId() != null) {
+            throw new Exception("El id debe de ser nulo");
+        }
+
+        if (judgeDTO.getName() == null || judgeDTO.getName().isBlank()) {
+            throw new Exception("El nombre no debe ser nulo o vacío");
+        }
+
+        if (judgeDTO.getRoleId() == null) {
+            throw new Exception("El RoleId no debe ser nulo o vacío");
+        }
+
+        Judge judge = JudgeMapper.dtoToDomain(judgeDTO);
+        JudgeRole judgeRole = judgeRoleRepository.getReferenceById(judgeDTO.getRoleId());
+
+        if (judgeRole == null){
+            throw new Exception("El Player no existe");
+        }
+
+        judge.setRole(judgeRole);
+        judgeRepository.save(judge);
+        return JudgeMapper.domainToDto(judge);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public JudgeDTO buscarJudgePorId(Integer id) throws Exception {
+        if(id == null || id.equals(0)) {
+            throw new Exception("El id no puede estar vacio ni ser 0");
+        }
+
+        Judge judge = judgeRepository.findById(id)
+                .orElseThrow (() -> new Exception("No se encuentra el judgde con el id" +id));
+        return JudgeMapper.domainToDto(judge);
+    }
+
+    @Override
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+    public JudgeDTO modificarJudge(JudgeDTO judgeDTO) throws Exception {
+        if(judgeDTO.getId() == null) {
+            throw new Exception("El id no debe de ser nulo");
+        }
+
+        if (judgeDTO.getName() == null || judgeDTO.getName().isBlank()) {
+            throw new Exception("El nombre no debe ser nulo o vacío");
+        }
+
+        if (judgeDTO.getRoleId() == null) {
+            throw new Exception("El RoleId no debe ser nulo o vacío");
+        }
+
+        Judge judge = JudgeMapper.dtoToDomain(judgeDTO);
+        JudgeRole judgeRole = judgeRoleRepository.getReferenceById(judgeDTO.getRoleId());
+
+        if (judgeRole == null){
+            throw new Exception("El Player no existe");
+        }
+
+        judge.setRole(judgeRole);
+        judgeRepository.save(judge);
+        return JudgeMapper.domainToDto(judge);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<JudgeDTO> obtenerJudges() {
+        List<Judge>listaJudges = judgeRepository.findAll();
+        List<JudgeDTO>judgesDTO = JudgeMapper.domainToDTOList(listaJudges);
+        return JudgeMapper.domainToDTOList(listaJudges);
+    }
+}
