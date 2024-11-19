15873b18f9b863dc2b48a69bffb52b72063ca03a
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2024-11-19 10:14:45 -0500

    Final corrections 3

--- src/main/java/com/liga/ligabetplay/service/MatchService.java
+++ src/main/java/com/liga/ligabetplay/service/MatchService.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public interface MatchService {
     MatchDTO guardarNuevoMatch(MatchDTO matchDTO) throws Exception;
-    MatchDTO buscarMatchPorId(Integer Id) throws Exception;
+    MatchDTO buscarMatchPorId(Integer id) throws Exception;
     MatchDTO modificarMatch(MatchDTO matchDTO) throws Exception;
     List<MatchDTO> obtenerMatches();
 }
